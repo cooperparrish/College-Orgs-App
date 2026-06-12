faf1be0b96d95d0cf82f7544943a368a10c5a645
cooperparrish <cparrish03888@gmail.com>
2026-06-12 18:54:19 -0400

    Update 0001_init.sql

--- supabase/0001_init.sql
+++ supabase/0001_init.sql
@@ -8,8 +8,16 @@ CREATE EXTENSION IF NOT EXISTS "citext";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Custom Enums
---go here
+CREATE TYPE public.org_type AS ENUM ('fraternity', 'sorority', 'club', 'organization');
+CREATE TYPE public.account_type AS ENUM ('student', 'alumni');
+CREATE TYPE public.affiliation_status AS ENUM ('current', 'past');
+CREATE TYPE public.verification_method AS ENUM ('email_otp', 'sso', 'sheerid', 'document');
+CREATE TYPE public.membership_role AS ENUM ('admin', 'user', 'alumni');
+CREATE TYPE public.membership_status AS ENUM ('pending', 'active', 'removed');
+CREATE TYPE public.invitation_status AS ENUM ('');
 
+
+  
 -- Anchor Tables 
 
 
